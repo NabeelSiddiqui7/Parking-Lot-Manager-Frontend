5cac624045bda19bd497ac0802fa35cbe10002a1
Nabeel <nabeel.sddq@gmail.com>
2023-03-27 17:43:02 -0400

    Styled tables

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,7 +7,7 @@ export default function Customer() {
 
 
   return (
-    <div className="h-screen" style={{ backgroundImage: `url(${bgImg})` }}>
+    <div className="h-screen w-screen" style={{ backgroundImage: `url(${bgImg})` }}>
 
       <HeaderMain />
 
@@ -16,22 +16,44 @@ export default function Customer() {
           <SearchBar />
         </div>
 
-        <div>
-        <table className="p-10 pl-10 ml-10 bg-white">
-          <tr>
-            <th>Name</th>
-            <th>Free Spots</th>
-            <th>Location</th>
+        <div className="m-10 w-1/2 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr className="p-5">
+              <th scope="col" className="px-6 py-3">Name</th>
+              <th scope="col" className="px-6 py-3">Free Spots</th>
+              <th scope="col" className="px-6 py-3">Location</th>
+            </tr>
+          </thead>
+          <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
           </tr>
-          <tr>
-            <td>Lorem Ipsum</td>
-            <td>Lorem Ipsum</td>
-            <td>Lorem Ipsum</td>
+          <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
           </tr>
-          <tr>
-            <td>Lorem Ipsum</td>
-            <td>Lorem Ipsum</td>
-            <td>Lorem Ipsum</td>
+          <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+          </tr>
+          <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+          </tr>
+          <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+          </tr>
+          <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2">Lorem Ipsum</td>
           </tr>
         </table>
       </div>
