ab6105f8ad6a1041b571945f98371b2d394ab9fb
Nabeel <nabeel.sddq@gmail.com>
2023-03-27 23:46:49 -0400

    Added filter button and further styling to parking lot list page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import { Link } from "react-router-dom";
 import bgImg from "../images/BgMain.svg";
 import { SearchBar } from "../components/SearchBar";
 import { HeaderMain } from "../components/HeaderMain";
+import {SlArrowRight} from "react-icons/sl"
 
 export default function Customer() {
 
@@ -11,49 +14,64 @@ export default function Customer() {
 
       <HeaderMain />
 
+      <div className="flex justify-center text-4xl md:text-5xl font-bold text-[#EFEFEF] mt-10 md:mt-16 mb-4">
+            Available Lots
+      </div>
+
       <div className="backdrop-blur-main h-full">
-        <div className="p-10 pl-10">
+        <div className="p-10 flex justify-center">
           <SearchBar />
+          <div className="flex flex-row items-center">
+            <p className="mx-5 text-white">Filter:</p>
+            <Select className="text-base text-black"/>
+          </div>
         </div>
 
-        <div className="m-10 w-1/2 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="m-auto w-2/3 relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr className="p-5">
-              <th scope="col" className="px-6 py-3">Name</th>
-              <th scope="col" className="px-6 py-3">Free Spots</th>
-              <th scope="col" className="px-6 py-3">Location</th>
+          <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-7">Name</th>
+              <th scope="col" className="px-6 py-7">Free Spots</th>
+              <th scope="col" className="px-6 py-7">Location</th>
+              <th scope="col" className="px-6 py-7 w-44">Occupancy</th>
             </tr>
           </thead>
           <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4"><div className="flex">Lorem Ipsum <Link className="pl-12" to="/"><SlArrowRight/></Link></div></td>
           </tr>
           <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4"><div className="flex">Lorem Ipsum <Link className="pl-12" to="/"><SlArrowRight/></Link></div></td>
           </tr>
           <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4"><div className="flex">Lorem Ipsum <Link className="pl-12" to="/"><SlArrowRight/></Link></div></td>
           </tr>
           <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4"><div className="flex">Lorem Ipsum <Link className="pl-12" to="/"><SlArrowRight/></Link></div></td>
           </tr>
           <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4"><div className="flex">Lorem Ipsum <Link className="pl-12" to="/"><SlArrowRight/></Link></div></td>
           </tr>
           <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
-            <td className="px-2">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4">Lorem Ipsum</td>
+            <td className="px-2 py-4"><div className="flex">Lorem Ipsum <Link className="pl-12" to="/"><SlArrowRight/></Link></div></td>
           </tr>
         </table>
       </div>
