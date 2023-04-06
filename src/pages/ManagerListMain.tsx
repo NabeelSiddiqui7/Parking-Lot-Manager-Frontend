df5eb93451457f6e9c58bb0dbd44873f4e41c009
Sawyer Hogeterp <shogeter@uwo.ca>
2023-04-06 16:21:37 -0400

    Add manager now correctly takes name

--- src/pages/ManagerListMain.tsx
+++ src/pages/ManagerListMain.tsx
@@ -64,7 +64,7 @@ function AddManagerModal(props: {isOpen:boolean, handleOpen: () => void, handleC
               <h2 className='mb-2'>Add Manager</h2>
               <form id='createTicket' onSubmit={onSubmitForm}>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
-                      <input type="text" name="first_name" onChange={handleInput} className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                      <input type="text" name="full_name" onChange={handleInput} className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
                       <input type="text" name="user_name" onChange={handleInput} className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
